887ae79594034d6e332d91f81763bba8db141903
Suraj Annadate <124056028+Suraj-Annadate@users.noreply.github.com>
2023-06-09 13:10:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,8 +2,18 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
+	for(let get in sampleObject)
+		{
+			if(get==key)
+			{
+				return true
+			}else
+			{
+				return false
+			}
+		}
 }
 
 // Do not change the code below
-const key = prompt("Enter Key.");
-alert(hasKey(key));
+// const key = prompt("Enter Key.");
+// alert(hasKey(key));
